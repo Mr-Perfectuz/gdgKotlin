d1a8533bd5748593db78d567151e41bdf9355e2a
Mr.Perfect <ziyodovanvar1999@gmail.com>
2020-09-23 19:25:20 +0500

    Coroutines Example in Kotlin

--- app/src/main/java/mrperfect/example/coroutineexample/MainActivity.kt
+++ app/src/main/java/mrperfect/example/coroutineexample/MainActivity.kt
@@ -2,19 +2,46 @@ package mrperfect.example.coroutineexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.os.IResultReceiver
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    private val RESULT_1 = "Result #1"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         button.setOnClickListener{
 
+            // IO, Main, Default
+            CoroutineScope(IO).launch {
+                fakeApiRequest()
+            }
+
         }
     }
 
-    private suspend fun getResult1FromApi() {
+    private fun setNewText(input: String) {
+        val newText = text.text.toString() + "\n$input"
+    }
+
+    private suspend fun fakeApiRequest() {
+        val result1 = getResult1FromApi()
+        println("debug: $result1")
+        text.setText(result1)
+    }
+
+    private suspend fun getResult1FromApi(): String {
+        logThread("getResult1FromApi")
+        delay(1000)
+        Thread.sleep(1000)
+        return RESULT_1
 
     }
 
